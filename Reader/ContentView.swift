cfe7e965f159dee2640a59ba1168e090c06ece85
Chaim Finkelman <toChaim@gmail.com>
2020-05-03 23:03:45 -0700

    Initial Commit

---
+++ Reader/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Reader
+//
+//  Created by Chaim Finkelman on 5/3/20.
+//  Copyright © 2020 Chaim Finkelman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
